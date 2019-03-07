c6b44096d83af49fabd7f3f0a8926ee79d82514d
3gtc <3gtc@3gtcdeMac-mini.local>
2019-03-07 11:55:41 +0800

    创建flask 小程序点餐项目

---
+++ config/local_setting.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql://root:/food_db?charset=utf8mb4'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
